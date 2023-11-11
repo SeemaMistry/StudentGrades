3b35183a3fb1c3992189b2fe1467f70eacf9b30e
SeemaMistry <seema.m.1996@gmail.com>
2023-11-11 17:50:04 -0500

    feat: create UserProfile class in models.py with 1:1 relation to account User

--- backend/user_profile/models.py
+++ backend/user_profile/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # each account is linked to one profile
+    
+    # set fields
+    first_name = models.CharField(max_length=225, default='')
+    last_name = models.CharField(max_length=225, default='')
+    phone = models.CharField(max_length=20, default='')
+    city = models.CharField(max_length=20, default='')
+
+    def __str__(self):
+        return self.first_name + self.last_name
