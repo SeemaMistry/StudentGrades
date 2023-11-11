87e1d0e40270b4ee4d8acd4c1b0a675572e15797
SeemaMistry <seema.m.1996@gmail.com>
2023-11-11 18:40:34 -0500

    fix: update path name from csrf_cookies to csrf_cookie

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import SignupView, GetCSRFToken
 urlpatterns = [
     path('register', SignupView.as_view()),
-    path('csrf_cookies', GetCSRFToken.as_view())
+    path('csrf_cookie', GetCSRFToken.as_view())
 ]
