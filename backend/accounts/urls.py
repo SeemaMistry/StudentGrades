14ff9f1d168f201135292cabee6a26e717b455de
SeemaMistry <seema.m.1996@gmail.com>
2023-11-11 18:08:04 -0500

    feat: set up url paths to register

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
-
-urlpatterns = []
+from .views import SignupView, GetCSRFToken
+urlpatterns = [
+    path('register', SignupView.as_view()),
+    path('csrf_cookies', GetCSRFToken.as_view())
+]
