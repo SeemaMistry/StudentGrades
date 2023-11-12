4b8df7b15336492e8f16938601c7cecddbd04039
SeemaMistry <seema.m.1996@gmail.com>
2023-11-12 18:55:21 -0500

    feat: add delete user and display all user url routes

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import SignupView, GetCSRFToken, LoginView, LogoutView, CheckAuthenticated
+from .views import SignupView, GetCSRFToken, LoginView, LogoutView, CheckAuthenticated, DeleteUserView, DisplayAllUsers
 urlpatterns = [
     path('register', SignupView.as_view()),
     path('csrf_cookie', GetCSRFToken.as_view()),
     path('login', LoginView.as_view()),
     path('logout', LogoutView.as_view()),
     path('authenticated', CheckAuthenticated.as_view()),
+    path('deleteUser',  DeleteUserView.as_view()),
+    path('getAllUsers', DisplayAllUsers.as_view())
 ]
