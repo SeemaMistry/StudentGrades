c5ae35144b492fcd482088d85314e7ed08660deb
SeemaMistry <seema.m.1996@gmail.com>
2023-11-14 12:49:19 -0500

    feat: create homepage jumbotron describing app with a login button

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='container'>
+      <div className='mt-5 p-5 bg-light'>
+        <h1 className='display-4'>Welcome to Student Grades</h1>
+        <p className='lead'>This is a Student Grades app with session authentication in React and Django</p>
+        <hr className='my-4'></hr>
+        <p>Click the button below to log in.</p>
+        <Link className='btn btn-primary btn-lg' to='/login'>Login</Link>
+      </div>
+    </div>
   )
 }
 
