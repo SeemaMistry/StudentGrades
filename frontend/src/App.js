5d8b0ef7c0b268327fc0e6f93549cac765e6335f
SeemaMistry <seema.m.1996@gmail.com>
2023-11-14 12:28:46 -0500

    fix: wrap all page <Route/> inside the parent <Routes/> component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Layout from './hocs/Layout'
 import Home from './containers/Home'
 import Register from './containers/Register'
@@ -11,10 +11,12 @@ const App = () => {
     return (
         <Router>
             <Layout>
-                <Route exact path='/' Component={Home}></Route>
-                <Route exact path='/register' Component={Register}></Route>
-                <Route exact path='/login' Component={Login}></Route>
-                <Route exact path='/dashboard' Component={Dashboard}></Route>
+                <Routes>
+                    <Route exact path='/' Component={Home}></Route>
+                    <Route exact path='/register' Component={Register}></Route>
+                    <Route exact path='/login' Component={Login}></Route>
+                    <Route exact path='/dashboard' Component={Dashboard}></Route>
+                </Routes>
             </Layout>
         </Router>
     )
