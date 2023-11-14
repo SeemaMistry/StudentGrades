0c44a0685619549968ea7f6db3f35579c8775b2f
SeemaMistry <seema.m.1996@gmail.com>
2023-11-14 12:02:38 -0500

    feat: set register, login, home, dashboard routes with React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react'
-
+import {BroswerRouter as Router, Route} from 'react-router-dom'
+import Layout from './hocs/Layout'
+import Home from './containers/Home'
+import Register from './containers/Register'
+import Login from './containers/Login'
+import Dashboard from './containers/Dashboard'
 
 
 const App = () => {
     return (
-        <div>
-            My Django-React App
-        </div>
+        <Router>
+            <Layout>
+                <Route exact path='/' Component={Home}></Route>
+                <Route exact path='/register' Component={Register}></Route>
+                <Route exact path='/login' Component={Login}></Route>
+                <Route exact path='/dashboard' Component={Dashboard}></Route>
+            </Layout>
+        </Router>
     )
 }
 
