c24c8ab16d69cb4e77bb650c910b01b547a86eda
SeemaMistry <seema.m.1996@gmail.com>
2023-11-16 10:40:59 -0500

    feat: create Register component with registration verification

--- frontend/src/containers/Register.js
+++ frontend/src/containers/Register.js
@@ -1,9 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { register} from '../actions/auth'
 
-const Register = () => {
+const Register = ({register}) => {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+    re_password: ''
+  })
+  const [accountCreated, setAccountCreated] = useState(false)
+
+  // destructure formData for easy use
+  const { username, password, re_password } = formData
+
+  const onChange = e => setFormData({...formData, [e.target.name]: e.target.value})
+
+  const onSubmit = e => {
+    e.preventDefault()
+
+    // validate user-inputs for registration
+    if (password === re_password) {
+      register(username, password, re_password)
+      setAccountCreated(true)
+    } else {
+      setAccountCreated(false)
+    }
+
+    if (accountCreated) {
+      <Redirect  to={'/'} />
+    }
+
+  }
+
   return (
-    <div>Register</div>
+    <div className='container mt-5'>
+      <h1>Register for an Account</h1>
+      <p>Create an account with our Student Grades application</p>
+      <form onSubmit={onSubmit}>
+        <div className='form-group'>
+          <label className='form-label'>Username: </label>
+          <input
+            className='form-control'
+            type='text'
+            placeholder='username'
+            onChange={onChange}
+            value={username}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <label className='form-label'>Password: </label>
+          <input
+            className='form-control'
+            type='password'
+            placeholder='password'
+            onChange={onChange}
+            value={password}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <label className='form-label'>Retype Password: </label>
+          <input
+            className='form-control'
+            type='password'
+            placeholder='password'
+            onChange={onChange}
+            value={re_password}
+            required
+          />
+        </div>
+
+      </form>
+    </div>
   )
 }
 
-export default Register;
+export default connect(null,{register})(Register);
